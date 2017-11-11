d83561a4789a81a7e0d141fedea34bc8740560c4
Michał Glajc <pozycjoner.net@gmail.com>
2017-11-11 17:39:18 +0100

    extension loading

--- src/Poznet/ImageOptimBundle/PoznetImageOptimBundle.php
+++ src/Poznet/ImageOptimBundle/PoznetImageOptimBundle.php
@@ -2,8 +2,13 @@
 
 namespace Poznet\ImageOptimBundle;
 
+use Poznet\ImageOptimBundle\DependencyInjection\PoznetImageOptimExtension;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class PoznetImageOptimBundle extends Bundle
 {
+    public function getContainerExtension()
+    {
+        return new PoznetImageOptimExtension();
+    }
 }
